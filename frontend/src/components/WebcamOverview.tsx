d9aba4a05f936a5ed555f83b63a48cd9035834bb
ngtrgiabaoB2012063 <ngtrgiabao251002@gmail.com>
2023-02-11 14:55:01 +0700

    BAO - add stream control

--- frontend/src/components/WebcamOverview.tsx
+++ frontend/src/components/WebcamOverview.tsx
@@ -1,23 +1,88 @@
 import React from "react";
 
-import Webcam from "react-webcam";
-
 const WebcamStreamCapture = () => {
-    const webcamRef = React.useRef<any>(null);
+    const [isAudio, setIsAudio] = React.useState<boolean>(true);
+    const [isVideo, setIsVideo] = React.useState<boolean>(true);
 
-    const videoConstraints = {
-        width: 1280,
-        height: 720,
-        facingMode: "user",
+    console.log(isAudio, isVideo);
+
+    const handleAudio = () => {
+        setIsAudio((isAudio) => !isAudio);
     };
 
+    const handleVideo = () => {
+        setIsVideo((isVideo) => !isVideo);
+    };
+
+    const video = React.useRef<any>(null);
+
+    const getUserMedia = navigator.mediaDevices.getUserMedia;
+
+    getUserMedia({
+        audio: isAudio,
+        video: isVideo,
+    }).then((stream) => {
+        // Changing the source of video to current stream.
+        if (video.current && isVideo) {
+            video.current.srcObject = stream;
+            video.current.play();
+        } else if (isVideo === false) {
+            
+        }
+    });
+
     return (
-        <Webcam
-            audio={false}
-            ref={webcamRef}
-            videoConstraints={videoConstraints}
-            className="bg-black w-[45rem] h-fit rounded-2xl"
-        />
+        <>
+            <div className="flex flex-col relative justify-center">
+                {isVideo ? (
+                    <video
+                        ref={video}
+                        className="bg-black w-[40rem] h-[30rem] rounded-2xl"
+                        style={{
+                            transform: "rotateY(180deg)",
+                        }}
+                    ></video>
+                ) : (
+                    <div className="bg-black w-[40rem] h-[30rem] rounded-2xl text-white flex justify-center items-center font-bold text-xl">
+                        You are not allow permission camera 😐
+                    </div>
+                )}
+
+                <div className="absolute bottom-4 left-1/2 -translate-x-1/2 w-fit flex justify-center">
+                    {isAudio ? (
+                        <button
+                            onClick={handleAudio}
+                            className="bg-white font-bold rounded-full w-[3rem] h-[3rem]"
+                        >
+                            <i className="fa-solid fa-microphone"></i>
+                        </button>
+                    ) : (
+                        <button
+                            onClick={handleAudio}
+                            className="text-white font-bold rounded-full w-[3rem] h-[3rem] bg-red-500 "
+                        >
+                            <i className="fa-solid fa-microphone-slash"></i>
+                        </button>
+                    )}
+
+                    {isVideo ? (
+                        <button
+                            onClick={handleVideo}
+                            className="bg-white font-bold rounded-full w-[3rem] h-[3rem] ml-6"
+                        >
+                            <i className="fa-solid fa-video"></i>
+                        </button>
+                    ) : (
+                        <button
+                            onClick={handleVideo}
+                            className="text-white font-bold rounded-full w-[3rem] h-[3rem] bg-red-500 ml-6"
+                        >
+                            <i className="fa-solid fa-video-slash"></i>
+                        </button>
+                    )}
+                </div>
+            </div>
+        </>
     );
 };
 
