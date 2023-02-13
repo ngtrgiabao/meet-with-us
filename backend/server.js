a52511b445486b8fde4da231ba1d9e70eaa4fcff
ngtrgiabaoB2012063 <ngtrgiabao251002@gmail.com>
2023-02-13 14:40:24 +0700

    change varible PORT in file server.js of backend

--- backend/server.js
+++ backend/server.js
@@ -4,14 +4,13 @@ const { Server } = require("socket.io");
 
 const app = require("./app");
 const { route } = require("./src/api/v1/user/user.route");
+const { port } = require("./src/config/database.config");
 const httpServer = createServer(app);
 
-const io = new Server(httpServer, {
-    /* options */
-});
+const io = new Server(httpServer, {});
 
 io.on("connection", (socket) => {
     console.log("hello");
 });
 
-httpServer.listen(3000);
+httpServer.listen(port);
