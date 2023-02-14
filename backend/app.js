c23be460ea2d0a8f71b0ee4ff9f4c143065a51ea
ngtrgiabaoB2012063 <ngtrgiabao251002@gmail.com>
2023-02-14 19:56:12 +0700

    BAO - add log error file and add transition when change page

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const logEvents = require("./src/api/v1/helper/log.helper");
+
 const middleWare = require("./src/api/v1/user/user.middleware");
-const router = require("./src/api/v1/user/user.route.js");
+const userRouter = require("./src/api/v1/user/user.route.js");
+
+const ApiError = require("./src/api.error");
 
 const corsOptions = {
     origin: "http://localhost:3000",
@@ -12,6 +16,17 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(middleWare);
 
-app.use("/api/v1", router);
+app.use("/api/v1", userRouter);
+
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) => {
+    logEvents(`${req.url}------${req.method}------${err.message}`);
+    return res.status(err.statusCode || 500).json({
+        msg: err.message || "Internal Server Error",
+    });
+});
 
 module.exports = app;
--- frontend/src/views/Home.tsx
+++ frontend/src/views/Home.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import gsap from "gsap";
+import { gsap } from "gsap";
 import Peer from "peerjs";
 
 import "../styles/index.css";
 
 import BackgroundVideo from "../layouts/Background";
 import PopupRoomId from "../components/PopupRoomId";
+import Transition from "../components/Transition";
 
 const bgImg = require("../assets/background/home.mp4");
 
@@ -49,92 +50,25 @@ const Home = () => {
         mixBlendMode: "",
     });
 
-    return (
-        <div
-            id="Home"
-            className="h-screen w-screen overflow-hidden relative flex justify-center items-center p-4"
-            onMouseMove={(e) => {
-                gsap.to(mouse.current, {
-                    top: e.clientY - 15,
-                    left: e.clientX + 15,
-                });
-            }}
-        >
-            <BackgroundVideo bgImg={bgImg} />
+    const home = gsap.timeline();
 
+    return (
+        <>
+            <Transition timeline={home} />
             <div
-                className="mt-[32rem] flex w-1/2 justify-center items-center"
+                id="Home"
+                className="h-screen w-screen overflow-hidden relative flex justify-center items-center p-4"
                 onMouseMove={(e) => {
                     gsap.to(mouse.current, {
                         top: e.clientY - 15,
-                        left: e.clientX + 5,
-
-                        width: "6rem",
-                        height: "6rem",
-
-                        mixBlendMode: "difference",
-                    });
-                }}
-                onMouseLeave={(e) => {
-                    gsap.to(mouse.current, {
-                        top: e.clientY - 15,
                         left: e.clientX + 15,
-
-                        width: "2rem",
-                        height: "2rem",
-
-                        mixBlendMode: "",
                     });
                 }}
             >
-                {inputValue ? (
-                    <Link
-                        to="/overview-camera"
-                        className="text-md uppercase font-bold p-2 rounded bg-[#2C2F77] text-white hover:opacity-95 animate__animated animate__bounceIn"
-                    >
-                        Tham gia phòng
-                    </Link>
-                ) : (
-                    <button
-                        className="text-md uppercase font-bold p-2 rounded bg-[#2C2F77] text-white animate__animated animate__bounceIn"
-                        onClick={() => {
-                            handleCreateIdRoom();
-                            handleActive();
-                        }}
-                    >
-                        Tạo phòng
-                    </button>
-                )}
+                <BackgroundVideo bgImg={bgImg} />
 
-                {/* Room input */}
-                <input
-                    type="text"
-                    placeholder="enter your link room here"
-                    className="text-lg uppercase font-bold outline outline-1 focus:outline-2 p-2 rounded animate__animated animate__fadeIn mx-4 flex-1"
-                    onChange={handleInput}
-                />
-
-                {/* Copy clipboard */}
-                {isCopied ? (
-                    <span className="text-white p-2 px-4 rounded-lg animate__animated animate__bounceIn">
-                        <i className="fa-solid fa-check text-xl text-green-500"></i>
-                    </span>
-                ) : (
-                    <CopyToClipboard
-                        text={inputValue}
-                        onCopy={handleCopyClipboard}
-                    >
-                        <span className="text-white cursor-pointer hover:text-white hover:bg-blue-400 p-2 px-4 rounded-lg animate__animated animate__bounceIn">
-                            <i className="fa-regular fa-clipboard text-xl"></i>
-                        </span>
-                    </CopyToClipboard>
-                )}
-            </div>
-
-            <div className="absolute bottom-4 left-4">
-                <Link
-                    to="/about"
-                    className="font-bold text-white hover:underline"
+                <div
+                    className="mt-[32rem] flex w-1/2 justify-center items-center"
                     onMouseMove={(e) => {
                         gsap.to(mouse.current, {
                             top: e.clientY - 15,
@@ -158,32 +92,104 @@ const Home = () => {
                         });
                     }}
                 >
-                    Tìm hiểu thêm
-                </Link>
-                <span className="ml-1 text-white">về chúng tôi</span>
+                    {inputValue ? (
+                        <Link
+                            to="/overview-camera"
+                            className="text-md uppercase font-bold p-2 rounded bg-[#2C2F77] text-white hover:opacity-95 animate__animated animate__bounceIn"
+                        >
+                            Tham gia phòng
+                        </Link>
+                    ) : (
+                        <button
+                            className="text-md uppercase font-bold p-2 rounded bg-[#2C2F77] text-white animate__animated animate__bounceIn"
+                            onClick={() => {
+                                handleCreateIdRoom();
+                                handleActive();
+                            }}
+                        >
+                            Tạo phòng
+                        </button>
+                    )}
+
+                    {/* Room input */}
+                    <input
+                        type="text"
+                        placeholder="enter your link room here"
+                        className="text-lg uppercase font-bold outline outline-1 focus:outline-2 p-2 rounded animate__animated animate__fadeIn mx-4 flex-1"
+                        onChange={handleInput}
+                    />
+
+                    {/* Copy clipboard */}
+                    {isCopied ? (
+                        <span className="text-white p-2 px-4 rounded-lg animate__animated animate__bounceIn">
+                            <i className="fa-solid fa-check text-xl text-green-500"></i>
+                        </span>
+                    ) : (
+                        <CopyToClipboard
+                            text={inputValue}
+                            onCopy={handleCopyClipboard}
+                        >
+                            <span className="text-white cursor-pointer hover:text-white hover:bg-blue-400 p-2 px-4 rounded-lg animate__animated animate__bounceIn">
+                                <i className="fa-regular fa-clipboard text-xl"></i>
+                            </span>
+                        </CopyToClipboard>
+                    )}
+                </div>
+
+                <div className="absolute bottom-4 left-4">
+                    <Link
+                        to="/about"
+                        className="font-bold text-white hover:underline"
+                        onMouseMove={(e) => {
+                            gsap.to(mouse.current, {
+                                top: e.clientY - 15,
+                                left: e.clientX + 5,
+
+                                width: "6rem",
+                                height: "6rem",
+
+                                mixBlendMode: "difference",
+                            });
+                        }}
+                        onMouseLeave={(e) => {
+                            gsap.to(mouse.current, {
+                                top: e.clientY - 15,
+                                left: e.clientX + 15,
+
+                                width: "2rem",
+                                height: "2rem",
+
+                                mixBlendMode: "",
+                            });
+                        }}
+                    >
+                        Tìm hiểu thêm
+                    </Link>
+                    <span className="ml-1 text-white">về chúng tôi</span>
+                </div>
+
+                {/* CURSOR */}
+                <div
+                    style={{
+                        width: mouse.current.width,
+                        height: mouse.current.height,
+
+                        top: mouse.current.x,
+                        left: mouse.current.y,
+                        position: "absolute",
+                    }}
+                    ref={mouse}
+                    className="rounded-full transition duration-150 overflow-hidden flex justify-center items-center bg-white z-[99]"
+                ></div>
+
+                {/* Popup */}
+                <PopupRoomId
+                    id={peerId}
+                    isActive={isActive}
+                    togglePopup={handleActive}
+                />
             </div>
-
-            {/* CURSOR */}
-            <div
-                style={{
-                    width: mouse.current.width,
-                    height: mouse.current.height,
-
-                    top: mouse.current.x,
-                    left: mouse.current.y,
-                    position: "absolute",
-                }}
-                ref={mouse}
-                className="rounded-full transition duration-150 overflow-hidden flex justify-center items-center bg-white z-[99]"
-            ></div>
-
-            {/* Popup */}
-            <PopupRoomId
-                id={peerId}
-                isActive={isActive}
-                togglePopup={handleActive}
-            />
-        </div>
+        </>
     );
 };
 
