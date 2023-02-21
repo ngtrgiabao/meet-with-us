f468700ede2480a953787874573a7e609d6ab6a8
ngtrgiabaoB2012063 <ngtrgiabao251002@gmail.com>
2023-02-21 15:47:32 +0700

    add sign in, sign up, navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,6 +7,8 @@ import About from "./views/About";
 import UserOverview from "./views/UserOverview";
 import Room from "./views/Room";
 import Register from "./views/Register";
+import Login from "./views/Login";
+
 
 import _404 from "./views/_404";
 
@@ -15,24 +17,27 @@ import "./styles/index.css";
 const socket = io("http://localhost:3001");
 
 function App() {
-    const send = () => {
+    React.useEffect(() => {
         socket.emit("send-msg", {
             msg: "hello from react",
         });
-    };
+        socket.on("server", (data) => {
+            console.log(data);
+        });
+    }, []);
 
     return (
         <div className="App bg-slate-900">
-            <button onClick={send} className="text-white font-bold text-xl">
-                click me
-            </button>
             <Routes>
                 <>
                     <Route path="/" element={<Home />} />
                     <Route path="/about" element={<About />} />
                     <Route path="/overview-camera" element={<UserOverview />} />
                     <Route path="/room" element={<Room />} />
+                </>
+                <>
                     <Route path="/register" element={<Register />} />
+                    <Route path="/login" element={<Login />} />
                 </>
                 <>
                     <Route path="/*" element={<_404 />} />
