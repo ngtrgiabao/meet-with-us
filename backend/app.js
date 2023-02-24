74eea310c29475d1370071cb4557701ab991bdc8
ngtrgiabaoB2012063 <ngtrgiabao251002@gmail.com>
2023-02-24 10:49:08 +0700

    create room id global

--- backend/app.js
+++ backend/app.js
@@ -14,6 +14,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 //Routes
+app.use("/api/v1", route.userRoute.get("/"));
 app.use("/api/v1/user", route.userRoute);
 app.use("/api/v1/room", route.roomRoute);
 
