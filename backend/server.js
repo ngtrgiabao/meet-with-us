2355dbafd19ae5dbfcaadafd506b02ca9835d3e3
Vuong Minh Dang <dangvuongminh25@gmail.com>
2023-02-28 14:47:20 +0700

    New commit

--- backend/server.js
+++ backend/server.js
@@ -9,43 +9,45 @@ const PORT = config.app.port;
 const httpServer = http.createServer(app);
 
 const io = new Server(httpServer, {
-    cors: {
-        origin: "http://localhost:3000",
-        methods: ["GET", "POST"],
-    },
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
 });
 
 app.get("/:room", (req, res) => {
-    res.render("room", {
-        roomID: req.params.room,
-    });
+  res.render("room", {
+    roomID: req.params.room,
+  });
 });
 
 io.on("connection", (socket) => {
-    console.log(`user connected socket: ${socket.id}`);
-    // Emit event fromt server to client
-    socket.emit("server", {
-        msg: "hello from server",
-    });
+  console.log(`user connected socket: ${socket.id}`);
+  // Emit event fromt server to client
+  socket.emit("server", {
+    msg: "hello from server",
+  });
 
-    socket.on("react", (data) => {
-        console.log(data);
-    });
-    socket.on("join-room", (data) => {
-        const { username, roomID } = data;
-        console.log("user connected a room:", roomID, "with name", username);
+  socket.on("react", (data) => {
+    console.log(data);
+  });
+  socket.on("join-room", (data) => {
+    const { username, roomID } = data;
+    console.log("user connected a room:", roomID, "with name", username);
 
-        socket.broadcast.emit("member-join", {
-            username,
-            roomID,
-        });
-    });
+    socket.join(roomID);
 
-    socket.on("disconnect", () => {
-        console.log(`user disconnected: ${socket.id}`);
+    socket.broadcast.emit("member-join", {
+      username,
+      roomID,
     });
+  });
+
+  socket.on("disconnect", () => {
+    console.log(`user disconnected: ${socket.id}`);
+  });
 });
 
 httpServer.listen(PORT, () => {
-    console.log("server connected");
+  console.log("server connected");
 });
