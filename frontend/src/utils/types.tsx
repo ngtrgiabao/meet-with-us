74102ae1b257f777342d6bae8a6406bf0ccc8034
ngtrgiabaoB2012063 <ngtrgiabao251002@gmail.com>
2023-03-24 23:12:37 +0700

    resolve cannot toggle webcam, mic in user overview page

--- frontend/src/utils/types.tsx
+++ frontend/src/utils/types.tsx
@@ -4,6 +4,13 @@ export type TBannerVideo = {
     bgImg: string;
 };
 
+export type TDeviceContext = {
+    isMic: boolean;
+    isWebcam: boolean;
+    toggleMic: () => void;
+    toggleWebcam: () => void;
+};
+
 export type TRoomContainer = {
     joined?: "JOINED" | "JOINING";
     participants: Map<string, MediaStream>;
