f606ebff7cdcfdb96f1f9ec8374f376fdac54b95
ngtrgiabaoB2012063 <ngtrgiabao251002@gmail.com>
2023-03-26 09:13:08 +0700

    fix echo voice for host room

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,4 @@
 const http = require("http");
-const { Server } = require("socket.io");
 
 const app = require("./app");
 const config = require("./src/config/database.config");
@@ -8,44 +7,12 @@ const PORT = config.app.port;
 
 const httpServer = http.createServer(app);
 
-const io = new Server(httpServer, {
-    cors: {
-        origin: "http://localhost:3000",
-        methods: ["GET", "POST"],
-    },
-});
-
 app.get("/:room", (req, res) => {
     res.render("room", {
         roomID: req.params.room,
     });
 });
 
-io.on("connection", (socket) => {
-    console.log("New client connected");
-
-    socket.on("join", (room) => {
-        socket.join(room);
-        console.log(`Member ${socket.id} joined room ${room}`);
-        socket.to(room).emit("user-connected", socket.id);
-    });
-    socket.on("signal", (data) => {
-        console.log(`Member ${socket.id} signaling to ${data.target}`);
-
-        socket.on("signal", (data) => {
-            console.log(`Member ${socket.id} signaling to ${data.target}`);
-            socket
-                .to(data.target)
-                .emit("signal", { sender: socket.id, signal: data.signal });
-        });
-
-        socket.on("disconnect", () => {
-            console.log(`Member ${socket.id} disconnected`);
-            socket.broadcast.emit("user-disconnected", socket.id);
-        });
-    });
-
-    httpServer.listen(PORT, () => {
-        console.log("server connected");
-    });
+httpServer.listen(PORT, () => {
+    console.log("server connected");
 });
